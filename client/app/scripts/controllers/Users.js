ce2cf02c688099115787c9d0e74bf6103e9e7e7a
alex hidalgo <aj1hidalgo@gmail.com>
2014-04-18 22:48:24 -0500

    User management integration

--- client/app/scripts/controllers/Users.js
+++ client/app/scripts/controllers/Users.js
@@ -1,34 +1,37 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('UsersCtrl', function ($scope) {
+  .controller('UsersCtrl', function ($scope, User) {
+    var users = User.get({}, function () {
+      $scope.users = users;
+    });
+
     $scope.add = { role: -1 };
     $scope.sorter = 'login';
 
-    $scope.users = [
-      { login: 'jwilson', password: '123', first: 'James', last: 'Wilson', role: 'admin'},
-      { login: 'dnetto', password: '123', first: 'Deigo', last: 'Netto', role: 'nurse'},
-      { login: 'kgillenwater', password: '123', first: 'Kent', last: 'Gillenwater', role: 'doctor'}
-    ];
-
     $scope.create = function () {
+      var user = new User($scope.add);
+      user.$save();
+
       $scope.users.push($scope.add);
       $scope.add = { userRole: -1 };
     };
 
     $scope.update = function (user) {
-      var data = {
-        login: user.login,
+      User.update({ id: user._id }, {
+        username: user.username,
         password: user.password,
-        first: user.first,
-        last: user.last,
+        first_name: user.first_name,
+        last_name: user.last_name,
         role: user.role
-      };
+      });
 
-      console.log('update', data);
+      user.editActive = false;
     };
 
     $scope.delete = function (user) {
-      _.remove($scope.users, { login: user.login });
+      User.remove({ id: user._id });
+
+      _.remove($scope.users, { _id: user._id });
     };
   });
